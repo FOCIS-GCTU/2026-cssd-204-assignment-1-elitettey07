82fb3753add38526ec207dfbaa14e9949024e4bf
elitettey07 <2425404327@live.gctu.edu.gh>
2026-05-31 22:13:41 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials using only print function and strings.
+# Assignment Number: 2
+#
+# Name: Twum Elisha Tettey
+# STUDENT ID: 2425404327
+# Email: 2425404327@live.gctu.edu.gh
+# Grader: Sir Augustus Buckman
+#
+# On my honor, Twum Elisha Tettey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #this function is going to print my initials first as small capital letters
+    #then large as large capital letters made up of a capital letters of the same kind.
+    print()
+    print("...ETT")
+    print()
+    print("...EEEEEEEEEEEE........TTTTTTTTTTTT........TTTTTTTTTTTT.....")
+    print("...EEEEEEEEEEEE........TTTTTTTTTTTT........TTTTTTTTTTTT.....")
+    print("...EE.......................TT..................TT..........")
+    print("...EE.......................TT..................TT..........")
+    print("...EEEEEEEEEEEE.............TT..................TT..........")
+    print("...EEEEEEEEEEEE.............TT..................TT..........")
+    print("...EE.......................TT..................TT..........")
+    print("...EE.......................TT..................TT..........")
+    print("...EEEEEEEEEEEE...**........TT.........**.......TT........**")
+    print("...EEEEEEEEEEEE...**........TT.........**.......TT........**")
+    print()
+   
+main()    
 
-
-
-main()
